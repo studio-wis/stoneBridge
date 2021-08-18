d7c88236ca8fd5f561033a38c3b8b60044e6a9ce
studiowis <88180558+studiowis@users.noreply.github.com>
2021-08-19 07:30:50 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,52 @@
 /* main.js */
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
 
-
 $(function() {
     $("#headers").load("common/header.html");
     $("#footers").load("common/footer.html");
+})
 
+window.onload = function() {
+
+    var modeSelect = document.getElementById("modeSelect");
+    if (modeSelect) {
+        modeSelect.addEventListener("click", () => {
+            const html = document.documentElement;
+
+            if (html.classList.contains("dark")) {
+                html.classList.remove("dark");
+                localStorage.setItem("darkTheme", "false");
+            } else {
+                html.classList.add("dark");
+                localStorage.setItem("darkTheme", "true");
+            }
+        });
+    }
+
+    const storedTheme = localStorage.getItem("darkTheme");
+
+    if (storedTheme !== null) {
+        if (storedTheme === "true") {
+            document.documentElement.classList.add("dark");
+            $("#open-menu .screen-mode").addClass("change");
+            $("#open-menu .screen-mode").find("i").text("dark_mode");
+        } else {
+            document.documentElement.classList.remove("dark");
+            $("#open-menu .screen-mode").removeClass("change");
+            $("#open-menu .screen-mode").find("i").text("circle");
+        }
+    }
 
     /* a 태그 경로 제거 */
     $('a[href="#"]').click(function(e) {
         e.preventDefault();
     })
 
-
-
-
-
     // header 
 
     $(document).on("click", "header .menu-btn", function() {
         $(this).toggleClass("close");
+        $("header .logo").toggleClass("close");
         $("#open-menu").slideToggle(300);
 
         if ($(this).hasClass("close")) {
@@ -59,7 +86,15 @@ $(function() {
         $(this).next().toggleClass("slide");
     });
 
+    $(".user-setting .cateBox .depth01 .menu01").click(function() {
 
+        if ($(".user-setting .cateBox .depth01 .menu01").children().next().hasClass("slide")) {
+            $(this).children().children().css("border-bottom", "none");
+        } else {
+            $(this).children().children().css("border-bottom", "1px solid #fff");
+        }
+
+    });
 
     $(document).mouseup(function(e) {
         var sel_hide = $(".cateBox");
@@ -76,38 +111,24 @@ $(function() {
 
     $("#crew .crew-box").click(function() {
         $(this).next().animate({ right: "0" }, 600, "easeInQuad");
-        // $(".popup .brief-info p").delay(600).animate({ opacity: "1" }, 500);
         $(".popup .fixed-element p").addClass("ani");
         $(".popup .fixed-element h1").addClass("ani");
+        $("header").addClass("fixed");
         $("header .logo").hide();
         $("header .back-btn").show();
+        $("header .menu-btn").addClass("change");
         $("html, body").addClass("hidden");
     });
 
     $(document).on("click", "header .back-btn", function() {
         $(".popup").animate({ right: "-100%" }, 600, "easeInQuad");
-        // $(".popup .brief-info p").delay(600).animate({ opacity: "0" }, 500);
         $(".popup .fixed-element p").removeClass("ani");
         $(".popup .fixed-element h1").removeClass("ani");
+        $("header").removeClass("fixed");
         $("header .back-btn").hide();
+        $("header .menu-btn").removeClass("change");
         $("header .logo").show();
         $("html, body").removeClass("hidden");
-    });
-
-
-    // news tab_menu
-
-    $("#news .tab-menu li").click(function() {
-        $("#news .tab-menu li").removeClass("on");
-        var tab_on = $(this).index();
-        $("#news .tab-menu li:eq(" + tab_on + ")").addClass("on");
-    });
-
-    $("#news .tab-menu li").hover(function() {
-        $("#news .tab-menu li").removeClass("on");
-    });
-    $("#news .tab-menu").mouseleave(function() {
-        $("#news .tab-menu li:eq(0)").addClass("on");
     });
 
     $(document).on("click", ".news-href header .back-btn", function() {
@@ -117,6 +138,22 @@ $(function() {
         $(this).prop("href", "index.html");
     });
 
+    // news tab_menu
+
+    $("#news .update .post-box").addClass("current");
+    $("#news .tab-menu li").on("click", function() {
+
+        var tag_active = $(this).attr("data-tab");
+        var tab_on = $(this).index();
+
+        $("#news .tab-menu li").removeClass("on");
+        $("#news .update .post-box").removeClass("current");
+
+
+        $("#news .tab-menu li:eq(" + tab_on + ")").addClass("on");
+        $("." + tag_active).addClass("current");
+
+    });
 
     // screen mode
 
@@ -124,28 +161,27 @@ $(function() {
         $(this).toggleClass("change");
         if ($("#open-menu .screen-mode").hasClass("change")) {
             $("#open-menu .screen-mode").find("i").text("dark_mode");
+
         } else {
             $("#open-menu .screen-mode").find("i").text("circle");
+            $(".wrap").removeClass("dark-mode");
         }
     });
 
 
-});
-$(function() {
+
+
     // pc 감지
     if (!isMobile) {
 
 
 
     }
-});
-
-$(function() {
     // mobile 감지
     if (isMobile) {
 
         $("#contact marquee").attr("scrollamount", "10");
 
-
     }
-});
+
+};
